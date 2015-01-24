cddb356b04954801b57043a99862d746f0696f7a
Nguyen Cao Nhat Linh <nhatlinh95@gmail.com>
2015-01-25 02:00:39 +0800

    Should do sth, add the google API

--- app/src/main/java/com/idk/psa/MainActivity.java
+++ app/src/main/java/com/idk/psa/MainActivity.java
@@ -1,29 +1,105 @@
 package com.idk.psa;
 
+import android.app.Dialog;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.IntentSender;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.LocalBroadcastManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.PendingResult;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.ActivityRecognition;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+    private TextView tvActivity;
+    // Request code to use when launching the resolution activity
+    private static final int REQUEST_RESOLVE_ERROR = 1001;
+    // Unique tag for the error dialog fragment
+    private static final String DIALOG_ERROR = "dialog_error";
+    private GoogleApiClient mGoogleApiClient;
+    // Bool to track whether the app is already resolving an error
+    private boolean mResolvingError = false;
+    private static final String STATE_RESOLVING_ERROR = "resolving_error";
+    private RunningReceiver mRunningReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        int resp = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        if(resp == ConnectionResult.SUCCESS){
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(ActivityRecognition.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .build();
+        }
+        else {
+            Toast.makeText(this, "Please install Google Play Service.", Toast.LENGTH_SHORT).show();
+        }
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new PlaceholderFragment())
                     .commit();
         }
-        MainService.startService(this);
+
+        tvActivity = (TextView) findViewById(R.id.activity);
+        mResolvingError = savedInstanceState != null
+                && savedInstanceState.getBoolean(STATE_RESOLVING_ERROR, false);
+        mRunningReceiver = new RunningReceiver();
+        registerReceiver(mRunningReceiver, new IntentFilter("com.idk.psa.ACTIVITY_RECOGNITION_DATA"));
+
     }
 
+    private class RunningReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Log.d(this.getClass().getSimpleName(),"received");
+            Toast.makeText(context, intent.getStringExtra("Activity"), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putBoolean(STATE_RESOLVING_ERROR, mResolvingError);
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if (!mResolvingError) {
+            mGoogleApiClient.connect();
+
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        mGoogleApiClient.disconnect();
+        unregisterReceiver(mRunningReceiver);
+        super.onStop();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -47,6 +123,90 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    @Override
+    public void onConnected(Bundle bundle) {
+        Intent intent = new Intent(this,MainService.class);
+        intent.setAction("com.idk.psa.action.START");
+        PendingIntent pendingIntent = PendingIntent.getService(this,0,intent,PendingIntent.FLAG_UPDATE_CURRENT);
+        PendingResult<Status> result = ActivityRecognition.ActivityRecognitionApi.requestActivityUpdates(mGoogleApiClient, 6000, pendingIntent);
+        Toast.makeText(this,"Running the thingy",Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult result) {
+        if (mResolvingError) {
+            // Already attempting to resolve an error.
+            return;
+        } else if (result.hasResolution()) {
+            try {
+                mResolvingError = true;
+                result.startResolutionForResult(this, REQUEST_RESOLVE_ERROR);
+            } catch (IntentSender.SendIntentException e) {
+                // There was an error with the resolution intent. Try again.
+                mGoogleApiClient.connect();
+            }
+        } else {
+            // Show dialog using GooglePlayServicesUtil.getErrorDialog()
+            showErrorDialog(result.getErrorCode());
+            mResolvingError = true;
+        }
+    }
+
+    // The rest of this code is all about building the error dialog
+
+    /* Creates a dialog for an error message */
+    private void showErrorDialog(int errorCode) {
+        // Create a fragment for the error dialog
+        ErrorDialogFragment dialogFragment = new ErrorDialogFragment();
+        // Pass the error that should be displayed
+        Bundle args = new Bundle();
+        args.putInt(DIALOG_ERROR, errorCode);
+        dialogFragment.setArguments(args);
+        dialogFragment.show(getSupportFragmentManager(), "errordialog");
+    }
+
+    /* Called from ErrorDialogFragment when the dialog is dismissed. */
+    public void onDialogDismissed() {
+        mResolvingError = false;
+    }
+
+    /* A fragment to display an error dialog */
+    public static class ErrorDialogFragment extends DialogFragment {
+        public ErrorDialogFragment() { }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            // Get the error code and retrieve the appropriate dialog
+            int errorCode = this.getArguments().getInt(DIALOG_ERROR);
+            return GooglePlayServicesUtil.getErrorDialog(errorCode,
+                    this.getActivity(), REQUEST_RESOLVE_ERROR);
+        }
+
+        @Override
+        public void onDismiss(DialogInterface dialog) {
+            ((MainActivity)getActivity()).onDialogDismissed();
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_RESOLVE_ERROR) {
+            mResolvingError = false;
+            if (resultCode == RESULT_OK) {
+                // Make sure the app is not already connected or attempting to connect
+                if (!mGoogleApiClient.isConnecting() &&
+                        !mGoogleApiClient.isConnected()) {
+                    mGoogleApiClient.connect();
+                }
+            }
+        }
+    }
     /**
      * A placeholder fragment containing a simple view.
      */
--- app/src/main/java/com/idk/psa/MainService.java
+++ app/src/main/java/com/idk/psa/MainService.java
@@ -1,6 +1,7 @@
 package com.idk.psa;
 
 import android.app.IntentService;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.hardware.Sensor;
@@ -9,6 +10,9 @@ import android.hardware.TriggerEvent;
 import android.hardware.TriggerEventListener;
 import android.util.Log;
 import android.widget.Toast;
+
+import com.google.android.gms.location.ActivityRecognitionResult;
+import com.google.android.gms.location.DetectedActivity;
 
 import java.util.ArrayList;
 import java.util.Date;
@@ -46,7 +50,8 @@ public class MainService extends IntentService {
         if (intent != null) {
             final String action = intent.getAction();
             if (ACTION_START.equals(action)) {
-                handleActionStart();
+                handleActionStart(intent);
+
             } else if (ACTION_BAZ.equals(action)) {
                 final String param1 = intent.getStringExtra(EXTRA_PARAM1);
                 final String param2 = intent.getStringExtra(EXTRA_PARAM2);
@@ -59,13 +64,69 @@ public class MainService extends IntentService {
      * Handle action Foo in the provided background thread with the provided
      * parameters.
      */
-    private void handleActionStart() {
-        SensorManager sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
-        Sensor runningSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_DETECTOR);
-        latestStepsTime = new ArrayList<>(5);
-        TriggerEventListener triggerEventListener = new StepDetectorEventListener();
-        sensorManager.requestTriggerSensor(triggerEventListener, runningSensor);
+    private void handleActionStart(Intent intent) {
+        Log.d(this.getClass().getSimpleName(),"Start START");
+        if (ActivityRecognitionResult.hasResult(intent)) {
+            Log.d(this.getClass().getSimpleName(),"hasResult");
+            ActivityRecognitionResult result = ActivityRecognitionResult.extractResult(intent);
+
+            // Get the most probable activity from the list of activities in the update
+            DetectedActivity mostProbableActivity = result.getMostProbableActivity();
+
+            // Get the type of activity
+            int activityType = mostProbableActivity.getType();
+
+            if (activityType == DetectedActivity.ON_FOOT) {
+                DetectedActivity betterActivity = walkingOrRunning(result.getProbableActivities());
+                if (null != betterActivity)
+                    mostProbableActivity = betterActivity;
+            }
+            if (mostProbableActivity.getType() == DetectedActivity.RUNNING) {
+                Intent i = new Intent("com.idk.psa.ACTIVITY_RECOGNITION_DATA");
+                i.putExtra("Activity", getType(result.getMostProbableActivity().getType()) );
+                i.putExtra("Confidence", result.getMostProbableActivity().getConfidence());
+                sendBroadcast(i);
+
+            }
+            Intent i = new Intent("com.idk.psa.ACTIVITY_RECOGNITION_DATA");
+            i.putExtra("Activity", "Doing sth" );
+            i.putExtra("Confidence", result.getMostProbableActivity().getConfidence());
+            sendBroadcast(i);
+            Log.d(this.getClass().getSimpleName(),"send broadcast");
+        }
     }
+
+    private String getType(int type){
+        if(type == DetectedActivity.UNKNOWN)
+            return "Unknown";
+        else if(type == DetectedActivity.IN_VEHICLE)
+            return "In Vehicle";
+        else if(type == DetectedActivity.ON_BICYCLE)
+            return "On Bicycle";
+        else if(type == DetectedActivity.ON_FOOT)
+            return "On Foot";
+        else if(type == DetectedActivity.STILL)
+            return "Still";
+        else if(type == DetectedActivity.TILTING)
+            return "Tilting";
+        else
+            return "";
+    }
+
+    private DetectedActivity walkingOrRunning(List<DetectedActivity> probableActivities) {
+        DetectedActivity myActivity = null;
+        int confidence = 0;
+        for (DetectedActivity activity : probableActivities) {
+            if (activity.getType() != DetectedActivity.RUNNING && activity.getType() != DetectedActivity.WALKING)
+                continue;
+
+            if (activity.getConfidence() > confidence)
+                myActivity = activity;
+        }
+
+        return myActivity;
+    }
+
 
     private void openRunningScreen() {
         //TODO
